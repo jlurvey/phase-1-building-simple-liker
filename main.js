456ec33162c837437dcc1702d4fb26ad2b7ffbd4
Joseph Lurvey <joeylurvey@gmail.com>
2023-05-26 21:55:50 -0400

    complete lab

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll('.like-glyph');
+const errorModal = document.getElementById('modal');
+const errorMessage = document.getElementById('modal-message');
 
+function displayErrorMessage(message) {
+  errorModal.classList.remove('hidden');
+  errorMessage.textContent = message;
+  setTimeout(() => errorModal.classList.add('hidden'), 3000);
+};
 
+function likeButtonHandler (event) {
+  const heart = event.target;
+  mimicServerCall()
+  .then(() => {
+    if (heart.textContent === EMPTY_HEART) {
+      heart.textContent = FULL_HEART;
+      heart.classList.add('activated-heart');
+    } else { 
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    };
+  })
+  .catch((error) => {
+    displayErrorMessage(error)
+  });
+};
 
+likeButtons.forEach((button) => {
+  button.addEventListener('click', likeButtonHandler);
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
